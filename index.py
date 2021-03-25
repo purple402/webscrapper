db787ad92c7896431491d047fa1aace2d8e47b28
Joy Lee <purple402@naver.com>
2021-03-25 21:48:41 +0900

    find max_page

--- index.py
+++ index.py
@@ -9,6 +9,10 @@ pagination = indeed_soup.find("div", {"class": "pagination"})
 
 links = pagination.find_all('a')
 pages = []
-for link in links:
-    pages.append(link.find("span"))
-print(pages[0:-1])
+for link in links[:-1]:
+    # int로 만들어주기 위해서는 숫자만 있어야하기 때문에
+    # 처음부터 마지막 item을 자르고 for loop를 실행한다
+    pages.append(int(link.string))
+    # <a>안에 <span> 하나만 있고, 그 안에 string이 유일하기 때문에
+    # link.find('span').string과 결과가 같다.
+max_page = pages[-1]
