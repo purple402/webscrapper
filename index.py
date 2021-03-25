c68f857289a84d60868187cfca780c8977ba99ba
Joy Lee <purple402@naver.com>
2021-03-25 21:38:21 +0900

    pages -> links

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
 
-pages = pagination.find_all('a')
+links = pagination.find_all('a')
 spans = []
-for page in pages:
-    spans.append(page.find("span"))
+for link in links:
+    spans.append(link.find("span"))
 print(spans[0:-1])
