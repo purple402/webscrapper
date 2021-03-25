31331f8485319f5f03a9c04ad2029f1ed1acb449
Joy Lee <purple402@naver.com>
2021-03-25 21:35:26 +0900

    find page numbers

--- index.py
+++ index.py
@@ -3,4 +3,12 @@ from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50")
 
-print(indeed_result)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+print(spans[0:-1])
