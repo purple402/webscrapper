6ef47e6b50c3a553862c8945f67110616e1ebb80
Joy Lee <purple402@naver.com>
2021-03-26 17:36:25 +0900

    find company in job

--- indeed.py
+++ indeed.py
@@ -32,5 +32,10 @@ def extract_indeed_jobs(last_page):
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
     for result in results:
         title = result.find("h2", {"class": "title"}).find("a")["title"]
-        print(title)
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            print(company_anchor.string)
+        else :
+            print(company.string)
     return jobs
