e04d0fed08724564892ee9629bbb26c01abc1482
Joy Lee <purple402@naver.com>
2021-03-29 16:33:03 +0900

    make get_last_page fnc

--- so.py
+++ so.py
@@ -1,3 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
+
+
+def get_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2]
+    print(last_page)
+
+
+def get_jobs():
+    last_page = get_last_page()
+    return []
