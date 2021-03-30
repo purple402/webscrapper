436e58ee74989f916b0a577357c0e07b13855def
Joy Lee <purple402@naver.com>
2021-03-30 15:25:32 +0900

    update get_last_page

--- so.py
+++ so.py
@@ -8,8 +8,8 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    last_page = pages[-2]
-    print(last_page)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
 
 
 def get_jobs():
