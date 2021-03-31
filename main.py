4bf8ef3b2ca6d456ede36deed653cda2e7c3fba7
Joy Lee <purple402@naver.com>
2021-03-31 16:34:33 +0900

    update report page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask("WebScrapper")
 
@@ -8,6 +8,11 @@ def home():
 
 @app.route("/report")
 def report():
-    return "this is the report"
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+    else:
+        return redirect("/")
+    return render_template("report.html", searching_by=word)
 
 app.run(host="127.0.0.1", debug=True)
