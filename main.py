a7bcee52cd2539340e4bba3575fe261379ab0934
Joy Lee <purple402@naver.com>
2021-03-31 17:42:14 +0900

    make database for search same keyword again

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask("WebScrapper")
 
+db = {}
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -11,6 +13,12 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
+        existing_jobs = db.get(word)
+        if existing_jobs:
+            jobs = existing_jobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
     return render_template("report.html", searching_by=word)
