b518cac0d67e6eca35fdcc30fd78896274c5da99
Joy Lee <purple402@naver.com>
2021-03-31 16:09:15 +0900

    make report page

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask("WebScrapper")
 def home():
     return render_template("index.html")
 
+@app.route("/report")
+def report():
+    return "this is the report"
+
 app.run(host="127.0.0.1", debug=True)
