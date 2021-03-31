ab8e5fba761504073a2211473d706e02547b7680
Joy Lee <purple402@naver.com>
2021-03-31 20:48:34 +0900

    make button for download file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-from so import get_jobs 
+from flask import Flask, render_template, request, redirect, send_file
+from so import get_jobs
+from save import save_to_file
 
 app = Flask("WebScrapper")
 
@@ -23,5 +24,21 @@ def report():
     else:
         return redirect("/")
     return render_template("report.html", searching_by=word, resultsNumber=len(jobs), jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv", mimetype="text/csv", as_attachment=True)
+    except:
+        return redirect("/")
 
 app.run(host="127.0.0.1", debug=True)
