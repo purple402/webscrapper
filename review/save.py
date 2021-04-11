d7917ccf1a6a379ca5f63e3fb6518cad99cbb770
Joy Lee <purple402@naver.com>
2021-04-11 21:47:42 +0900

    modify file encoding error

--- review/save.py
+++ review/save.py
@@ -1,7 +1,7 @@
 import csv
 
 def save_to_file(word, jobs):
-    file = open(f"{word}.csv", 'w')
+    file = open(f"{word}.csv", 'w', encoding="UTF-8")
     writer = csv.writer(file)
     writer.writerow(["site", "title", "company", "link"])
     for job in jobs:
